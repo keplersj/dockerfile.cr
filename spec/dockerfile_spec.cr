c666b372609ed72f9cf67c0e6ca39d1c8e4d1668
Kepler Sticka-Jones <kepler+git@stickajones.org>
2016-06-30 13:31:40 -0600

    Create more extensive testing

--- spec/dockerfile_spec.cr
+++ spec/dockerfile_spec.cr
@@ -1,27 +1,37 @@
 require "./spec_helper"
 
 describe DockerfileParser do
-  dockerfile = DockerfileParser.load("#{__DIR__}/fixture/Dockerfile")
+  describe "parsing the example file" do
+    dockerfile = DockerfileParser.load("#{__DIR__}/fixture/Dockerfile.example")
 
-  it "follows the example" do
-    dockerfile.should eq([
-        {command: "FROM",       params: ["debian", "jessie"]},
-        {command: "MAINTAINER", params: "Nikolay Yurin <yurinnick@outlook.com>"},
-        {command: "RUN",        params: [
-                                  ["apt-get", "update"],["&"],
-                                  ["apt-get", "install", "-y", "nginx"]]},
-        {command: "RUN",        params: [
-                                  ["rm", "-rf", "/var/lib/apt/lists/*"],
-                                  ["&"],
-                                  ["chown", "-R", "www-data:www-data", "/var/lib/nginx"]]},
-        {command: "VOLUME",     params: "/var/www/html"},
-        {command: "WORKDIR",    params: "/etc/nginx"},
-        {command: "COPY",       params: {src: "site-example.conf",
-                                           dst: "/etc/nginx/sites-available/site-example.conf"}},
-        {command: "COPY",       params: {src: "index.html.tmpl",
-                                           dst: "/var/www/html/index.html"}},
-        {command: "EXPOSE",     params: "80"},
-        {command: "CMD",        params: ["nginx", "-g", "daemon off;"]}
-    ])
+    it "has a predictable output" do
+      dockerfile.should eq([
+          {command: "FROM",       params: ["debian", "jessie"]},
+          {command: "MAINTAINER", params: "Nikolay Yurin <yurinnick@outlook.com>"},
+          {command: "RUN",        params: [
+                                    ["apt-get", "update"],["&"],
+                                    ["apt-get", "install", "-y", "nginx"]]},
+          {command: "RUN",        params: [
+                                    ["rm", "-rf", "/var/lib/apt/lists/*"],
+                                    ["&"],
+                                    ["chown", "-R", "www-data:www-data", "/var/lib/nginx"]]},
+          {command: "VOLUME",     params: "/var/www/html"},
+          {command: "WORKDIR",    params: "/etc/nginx"},
+          {command: "COPY",       params: {src: "site-example.conf",
+                                             dst: "/etc/nginx/sites-available/site-example.conf"}},
+          {command: "COPY",       params: {src: "index.html.tmpl",
+                                             dst: "/var/www/html/index.html"}},
+          {command: "EXPOSE",     params: "80"},
+          {command: "CMD",        params: ["nginx", "-g", "daemon off;"]}
+      ])
+    end
+  end
+
+  describe "parsing an empty Dockerfile" do
+    docerfile = DockerfileParser.load("#{__DIR__}/fixture/Dockerfile.empty")
+
+    it "has a predictable output" do
+      docerfile.should eq([] of NamedTuple(command: String))
+    end
   end
 end
