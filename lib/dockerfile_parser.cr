aacda88353869e27424571a98eef616a25a2696e
Kepler Sticka-Jones <kepler+git@stickajones.org>
2016-06-29 23:35:25 -0600

    Ported to Crystal.

--- lib/dockerfile_parser.cr
+++ lib/dockerfile_parser.cr
@@ -1,7 +1,8 @@
 # Encoding: utf-8
 # @author Nikolay Yurin <yurinnick@outlook.com>
+# Ported to Crystal by Kepler Sticka-Jones <kepler@stickajones.org>
 
-require 'yaml'
+require "yaml"
 
 # DockerfileParser main class
 class DockerfileParser
@@ -17,17 +18,17 @@ class DockerfileParser
     end
   end
 
-  def self.split_dockerfile(str)
-    str.gsub(/(\s\\\s)+/i, '').gsub("\n", ' ').squeeze(' ').split(' ')
+  private def self.split_dockerfile(str)
+    str.gsub(/(\s\\\s)+/i, "").gsub("\n", " ").squeeze(" ").split(" ")
   end
 
-  def self.parse_commands(dockerfile_array)
+  private def self.parse_commands(dockerfile_array)
     dockerfile_array.each_with_index.map do |cmd, index|
       { index: index, command: cmd } if @commands.include?(cmd)
-    end.compact! << { index: dockerfile_array.length, command: 'EOF' }
+    end.compact! << { index: dockerfile_array.length, command: "EOF" }
   end
 
-  def self.process_steps(dockerfile_array, step, next_cmd_index)
+  private def self.process_steps(dockerfile_array, step, next_cmd_index)
     { command: step[:command],
       params: split_params(
         step[:command],
@@ -36,21 +37,16 @@ class DockerfileParser
     }
   end
 
-  def self.split_params(cmd, params)
+  private def self.split_params(cmd, params)
     case cmd
-    when 'FROM' then params.join('').split(':')
-    when 'RUN' then params.join(' ').split(/\s(\&|\;)+\s/).map(&:strip)
-    when 'ENV' then
-      { name: params[0], value: params[1..-1].join(' ') }
-    when 'COPY', 'ADD' then { src: params[0], dst: params[1] }
+    when "FROM" then params.join("").split(":")
+    when "RUN" then params.join(" ").split(/\s(\&|\;)+\s/).map(&:strip)
+    when "ENV" then
+      { name: params[0], value: params[1..-1].join(" ") }
+    when "COPY", "ADD" then { src: params[0], dst: params[1] }
     else
       params = params.join(' ') if params.is_a?(Array)
       YAML.load(params.to_s)
     end
   end
-
-  private_class_method :parse_commands
-  private_class_method :process_steps
-  private_class_method :split_params
-  private_class_method :split_dockerfile
 end
